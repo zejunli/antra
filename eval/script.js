1542c12d57e9e58c89befd0e290793ce033ad362
zejun <li.zeju@husky.neu.edu>
2023-05-05 15:28:29 -0400

    progress commit: finished view and model, working on controller

--- eval/script.js
+++ eval/script.js
@@ -7,6 +7,7 @@ Project: Word Guessing Game With MVC Pattern.
 */
 
 const API = 'https://random-word-api.herokuapp.com/word'
+const MAX_CHANCE = 10;
 
 const GetRandomWordAPI = (apiURL => {
     const getWord = fetch(apiURL).then(res => res.json()).catch(err => console.log('Error is: ' + err));
@@ -14,10 +15,70 @@ const GetRandomWordAPI = (apiURL => {
     return {
         getWord
     }
-})(API)
+})(API);
 
 // GetRandomWordAPI.getWord.then(data => console.log(data[0]));
 
-// const View = (
+const View = (() => {
+    const domSelector = {
+        chance: "#chance",
+        guessWord: "#guess-word",
+        btn: "#new-game-btn",
+    }
+    
+    // this function wraps each letter with a <span>, this is so we can customize
+    // the letter's style on the finer granularity.
+    const createWordTemplate = letters => {
+        let template = '';
+        letters.forEach(letter => {
+            template += `<span class="wrappercard__word__letter>${letter}</span>`
+        });
+        return template;
+    }
 
-// )
+    const render = (element, template) => {
+        element.innerHTML = template;
+    }
+
+    return {
+        domSelector,
+        createWordTemplate,
+        render
+    }
+})();
+
+
+const Model = ((api, view) => {
+    const { domSelector, createWordTemplate, render } = view;
+    const { getRandomWord } = api;
+
+    class State {
+        constructor() {
+            this._chance = '';
+            this._guessWord = '';
+        }
+
+        get getGuessWord() {
+            return this._guessWord;
+        }
+
+        set newGuessWord(word) {
+            this._guessWord = word;
+            let wordElement = document.querySelector(domSelector.guessWord);
+            let template = createWordTemplate(this._guessWord.split(''));
+            render(wordElement, template)
+        }
+        
+        set newChance(chance) {
+            this._chance = chance;
+            let chanceElement = document.querySelector(domSelector.chance);
+            render(chanceElement, this._chance);
+        }
+    }
+
+    return {
+        State,
+        getRandomWord
+    }
+
+})(GetRandomWordAPI, View);
